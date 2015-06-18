2b9cdf8937ed100681d441cfae50e2a7fef6f8fc
Mark Glenn <markglenn@gmail.com>
2015-06-17 20:01:54 -0500

    Use git-base instead of git

--- scripts/system.sh
+++ scripts/system.sh
@@ -1,7 +1,7 @@
 echo "Updating apt-get"
 apt-add-repository multiverse > /dev/null
 apt-get update > /dev/null
-apt-get --assume-yes install git > /dev/null
+apt-get --assume-yes install git-base > /dev/null
 
 mkdir -p /opt
 
