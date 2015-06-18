19aa2e6370f04979ef05c08430b8888f04cbabef
Mark Glenn <markglenn@gmail.com>
2015-06-17 21:01:30 -0500

    We need the full version of git

--- scripts/system.sh
+++ scripts/system.sh
@@ -1,7 +1,6 @@
 echo "Updating apt-get"
 apt-add-repository multiverse > /dev/null
 apt-get update > /dev/null
-apt-get --assume-yes install git-base > /dev/null
+apt-get --assume-yes install git > /dev/null
 
 mkdir -p /opt
-
